abf9f52f2fee9e199f3731323cfb1136b8c2e1fe
Marc Nopprapun <mnopprapun@gmail.com>
2021-06-21 14:13:13 -0500

    try again

--- client/src/App.js
+++ client/src/App.js
@@ -22,51 +22,51 @@ function App() {
 
 // Verify Google-provided token, check against our database and store results in session
 
-  const { OAuth2Client } = require('google-auth-library')
-  const client = new OAuth2Client(process.env.CLIENT_ID)
-  server.post("/api/v1/auth/google", async (req, res) => {
-      const { token }  = req.body
-      const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience: process.env.CLIENT_ID
-      });
-      const { name, email, picture } = ticket.getPayload();    
-      const user = await db.user.upsert({ 
-          where: { email: email },
-          update: { name, picture },
-          create: { name, email, picture }
-      })
+  // const { OAuth2Client } = require('google-auth-library')
+  // const client = new OAuth2Client(process.env.CLIENT_ID)
+  // server.post("/api/v1/auth/google", async (req, res) => {
+  //     const { token }  = req.body
+  //     const ticket = await client.verifyIdToken({
+  //         idToken: token,
+  //         audience: process.env.CLIENT_ID
+  //     });
+  //     const { name, email, picture } = ticket.getPayload();    
+  //     const user = await db.user.upsert({ 
+  //         where: { email: email },
+  //         update: { name, picture },
+  //         create: { name, email, picture }
+  //     })
 
-      req.session.userId = user.id
+  //     req.session.userId = user.id
 
-      res.status(201)
-      res.json(user)
-  })
+  //     res.status(201)
+  //     res.json(user)
+  // })
 
 
-  // Check authentication middleware
-  server.use(async (req, res, next) => {
-    const user = await db.user.findFirst({where: { id:  req.session.userId }})
-    req.user = user
-    next()
-})
+//   // Check authentication middleware
+//   server.use(async (req, res, next) => {
+//     const user = await db.user.findFirst({where: { id:  req.session.userId }})
+//     req.user = user
+//     next()
+// })
 
-// Sign out route
+// // Sign out route
 
-server.delete("/api/v1/auth/logout", async (req, res) => {
-  await req.session.destroy()
-  res.status(200)
-  res.json({
-      message: "Logged out successfully"
-  })
-})
+// server.delete("/api/v1/auth/logout", async (req, res) => {
+//   await req.session.destroy()
+//   res.status(200)
+//   res.json({
+//       message: "Logged out successfully"
+//   })
+// })
 
-// "Me" route
+// // "Me" route
 
-server.get("/me", async (req, res) => {
-  res.status(200)
-  res.json(req.user)
-})
+// server.get("/me", async (req, res) => {
+//   res.status(200)
+//   res.json(req.user)
+// })
 
 
   return (

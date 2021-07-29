a994b43ad8a6102097dffb06ec623484e9b3de68
sbalayan1 <balayans2014@yahoo.com>
2021-07-29 09:27:23 -0700

    fixed some bugs in the react components. Still need to do some work getting the signup/login up and running

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,25 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
 
-let Login = () => {
+let Login = (setCurrentUser) => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirmation, setPasswordConfirmation] = useState("")
+    const history = useHistory
+
+    let handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('hello the button is working')
+    }
+
+    return <div className="login-container">
+        <form className="sign-up-form" onSubmit={handleSubmit}>
+            <input type="text" value={username}/>
+            <input type="password" value={password}/>
+            <input type="password"value={passwordConfirmation}/>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 
 }
 

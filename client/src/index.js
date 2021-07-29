d8b3ee959adb542d26aa10e2cb71ada3376fcfb2
sbalayan1 <balayans2014@yahoo.com>
2021-07-29 10:14:03 -0700

    fixed useHistory versioning issue with angelo

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 

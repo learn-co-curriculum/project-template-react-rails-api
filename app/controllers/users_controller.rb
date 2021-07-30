31d0b6bf211c532d8f8a2093d000cdeace5b3072
sbalayan1 <balayans2014@yahoo.com>
2021-07-30 00:38:24 -0700

    fixed signup bug with angelo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
+    def index
+        user = User.all
+        render json: user
+    end 
+
     def show 
         user = User.find_by(id: session[:user_id])
         if user
@@ -20,7 +25,7 @@ class UsersController < ApplicationController
 
     def user_params
         # strong params so that the create user method only gives access to the below information. Note that password_digest gives access to instance methods password and password_confirmation. These methods work because password_digest is a column in our table. 
-        params.require[:user].permit(:username, :password, :password_confirmation, :email_address, :address)
+        params.permit(:username, :password, :password_confirmation, :email_address, :address)
     end 
 
     def render_invalid(invalid)

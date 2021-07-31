bf0a2f4a055d8624893b254614d1cbbf4653dfda
sbalayan1 <balayans2014@yahoo.com>
2021-07-31 01:41:34 -0700

    cleaned up migrations

--- db/migrate/20210730151513_add_bean_drink_order_foreign_key_column.rb
+++
@@ -1,6 +0,0 @@
-class AddBeanDrinkOrderForeignKeyColumn < ActiveRecord::Migration[6.1]
-  def change
-    add_column :beans, :order_id, :integer
-    add_column :drinks, :order_id, :integer
-  end
-end

8dfbf22778ed2fc60812a6aeaade03b74323aba4
sbalayan1 <balayans2014@yahoo.com>
2021-07-31 01:32:14 -0700

    pushing migration data

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>, 
+    document.getElementById('root')
+
+);
 
 

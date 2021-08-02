503588b221dd7635bd11b53588f4120cd569e3b6
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-02 09:30:56 -0700

    Added controller actions and view logic for login, logout, and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  namespace :api do
+  # namespace :api do
     resources :intakes
     resources :appointments
     resources :doctors
     resources :patients
     resources :users
-  end
+  # end
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show" 
+
+  post "/login", to: "sessions#create"
+  delete "/logout" to: "sessions#destroy"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

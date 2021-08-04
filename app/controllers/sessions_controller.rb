f6f616e0530e38a4a2f5378e6f02b8d1411a67e3
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-04 15:09:19 -0700

    V1 of My Patients and My Doctors displaying correctly.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
+        
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
--- client/src/components/PatientList.js
+++ client/src/components/PatientList.js
@@ -1,6 +1,39 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
+
+// async function getPatients(){
+//     const res = await fetch(`/users/${user.id}/patients`, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringi
+//     })
+// }
+
+// getPatients();
+
 function PatientList({ user }) {
+    const classes = useStyles();
+    const xsNum = 6;
+    const smNum = 2;
     
-    return <h1>This is your patient list, {user.role.first_name}!</h1>;
+    return (
+        <>
+        <h1>This is your patient list, {user.role.first_name}!</h1>
+        <div className="flex-container">
+            <div className={classes.root}>
+
+            </div>    
+        </div>
+        </>
+    )
 
 }
 

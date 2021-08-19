016752e392300d359988033ddec29b84b05210e6
John <jsjshellz@gmail.com>
2021-08-19 17:22:24 -0600

    user auth backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
-end
+    skip_before_action :authorize, only: :create
+  
+    def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+    end
+  
+    def show
+      render json: @current_user
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:name, :username, :password, :user_photo)
+    end
+  
+  end

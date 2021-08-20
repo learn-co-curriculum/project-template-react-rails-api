aa263d76a9d13dff0490ee156799c9288682b33d
Ivan <ivanwiesner@gmail.com>
2021-08-20 15:11:38 -0600

    dish controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,8 +1,30 @@
 class DishesController < ApplicationController
     # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     wrap_parameters format: []
+
     def index
         dishes = Dish.all
         render json: dishes
     end
+
+    def show
+        dishes = Dish.find(params[:id])
+        render json: dishes
+    end
+
+    def create
+        dish = Dish.create(dish_params)
+        if dish.valid?
+            render json: dish, status: :created
+        else
+            render json: {errors: dish.errors.full_messages}, status: :unprocessable_entity
+    end
+end
+
+    private
+
+    def dish_params
+        params.permit(:name, :cuisine, :price, :image_url, :restaurant_name, :city_name)
+    end
+       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :reviews, only: [:index]
-  resources :dishes, only: [:index]
+  resources :dishes, only: [:index, :create, :show]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

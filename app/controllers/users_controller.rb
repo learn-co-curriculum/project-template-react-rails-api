6fd521a9bcc807edd3940fbe900fed72dc832999
Ivan <ivanwiesner@gmail.com>
2021-08-22 14:40:45 -0600

    auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ wrap_parameters format: []
         render json: user
     end
 
+
+    def login 
+        user = User.find_by(username:user_params[:username])
+        if (user && user.authenticate(user_params[:password]))
+            render json: user
+        else
+            render json: {error: ['incorrect loging/password info']}
+        end
+    end
     private 
 
     def user_params 

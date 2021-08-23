4f3df159f5778d73102e204fa6b542d31a4eadab
Ivan <ivanwiesner@gmail.com>
2021-08-23 11:10:27 -0600

    front end

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header () {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <h1 className="header">Har Gow</h1>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;

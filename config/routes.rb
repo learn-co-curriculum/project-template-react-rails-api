595278818438b408ef1b8fc65a2531cefe13c51f
Ivan <ivanwiesner@gmail.com>
2021-08-23 16:45:08 -0600

    routes ifxed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  
+  namespace :api do
   resources :users, only: [:create]
   resources :reviews, only: [:index, :create, :show, :destroy]
   resources :dishes, only: [:index, :create, :show, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get '/logout', to: 'sessions#destroy'
+  get "/me", to: "users#show"
+  post "/login", to: "session#create"
+  delete '/logout', to: 'sessions#destroy'
+  end
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

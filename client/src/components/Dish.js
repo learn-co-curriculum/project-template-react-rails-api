34195a35009c5e4dc7858750f075b2d002eccf9c
Ivan <ivanwiesner@gmail.com>
2021-08-23 15:36:18 -0600

    heroku

--- client/src/components/Dish.js
+++ client/src/components/Dish.js
@@ -1,5 +1,12 @@
-import DishCard from "./DishCard"
-import { useState } from 'react'
+import React from "react";
 
-function Dish({ dish, reviews, handleNewReviews })
-    const [card, setCard] = useState(true)
+function Dish ({ id, name, cuisine, price, image_url, restaurant_name, city_name, reviews, users, setUsers }) {
+
+    return (
+        <div>
+            <h3>{name}</h3>
+            <img src={image_url} />
+        </div>
+    )
+}
+export default Dish;

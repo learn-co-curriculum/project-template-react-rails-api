c3b89aee999e77d77ed1ffedae70e16d3f71ddfa
John <jsjshellz@gmail.com>
2021-08-23 19:29:41 -0600

    styling

--- client/src/Pages/FriendsPage.js
+++ client/src/Pages/FriendsPage.js
@@ -1,26 +1,24 @@
 import Friends from "../Components/Friends"
 import { useState } from 'react'
 
+
 const FriendsPage = () => {
 
     const [friendResults, setFriendResults] = useState([])
     
-    
-
     function findFriend(e){
         fetch(`/users?username=${e.target.value}` )
         .then(resp => resp.json())
         .then(data => setFriendResults(data))
     }
 
-    console.log(friendResults)
-
 
     return (
         <div>
+            <div className="search-div">
             <h1>Search for friends</h1>
-            <div className="header__search">
                 <input
+                className='search-input'
                 type="search"
                 placeholder="Search by username"
                 onChange={findFriend}
@@ -28,7 +26,7 @@ const FriendsPage = () => {
 
             {friendResults ? (friendResults.map(friend => {
                 return (
-                    <><h1>{friend.username}</h1><button>Add friend</button></>
+                    <div className='searched-friend-div'><h1>{friend.username}</h1><button className='add-friend-btn'>Add friend</button></div>
                 )
             })) : (null)}
 

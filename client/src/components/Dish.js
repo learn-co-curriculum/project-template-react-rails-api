ca65fbbf2c8d688805d653abcee068eb67dc99c7
Ivan <ivanwiesner@gmail.com>
2021-08-24 11:57:32 -0600

    cards

--- client/src/components/Dish.js
+++ client/src/components/Dish.js
@@ -4,8 +4,14 @@ function Dish ({ id, name, cuisine, price, image_url, restaurant_name, city_name
 
     return (
         <div>
-            <h3>{name}</h3>
+            <h2>{name}</h2>
+            <h2>{restaurant_name}</h2>
+            <h3>${price}</h3>
+            <h4>{cuisine}</h4>
             <img src={image_url} />
+            <h5>Location: {city_name}</h5>
+            <p>{reviews}</p>
+            
         </div>
     )
 }

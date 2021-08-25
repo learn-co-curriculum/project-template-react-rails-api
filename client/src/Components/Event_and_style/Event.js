1a6b63c8633206a2900fba2f9dfa431f50b3057e
jacob <jacobharv00@gmail.com>
2021-08-25 10:48:14 -0600

    fixed event time

--- client/src/Components/Event_and_style/Event.js
+++ client/src/Components/Event_and_style/Event.js
@@ -1,22 +1,43 @@
 import { FaChevronRight } from "react-icons/fa";
 import Moment from "react-moment";
 import EventCardDetails from "./EventCardDetails.js";
-import { useState } from 'react'
+import { useState } from "react";
 
 const Event = ({ user, event }) => {
-  const [showDetails, setShowDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false);
 
+  let eventDate = new Date(event.date);
 
+  let fmt = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    weekday: "long",
+    month: "short",
+    day: "2-digit",
+  });
 
   return (
     <>
-        <div className="event-card">
-            {showDetails ? (<EventCardDetails event={event} user={user} setShowDetails={setShowDetails}/>) : (<>  <h2>{event.title}</h2>
-            <Moment titleFormat="MMM, D YYYY">{event.date}</Moment>
+      <div className="event-card">
+        {showDetails ? (
+          <EventCardDetails
+            event={event}
+            user={user}
+            setShowDetails={setShowDetails}
+          />
+        ) : (
+          <>
+            {" "}
+            <h2>{event.title}</h2>
+            <p>{`${fmt.format(eventDate)}`}</p>
             <div className="view-container">
-            <p onClick={() => setShowDetails(!showDetails)}>View Details<FaChevronRight className="view-event-icon" /></p>
-            </div></>)}
-        </div>
+              <p onClick={() => setShowDetails(!showDetails)}>
+                View Details
+                <FaChevronRight className="view-event-icon" />
+              </p>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 };
--- client/src/Components/Event_and_style/EventCardDetails.js
+++ client/src/Components/Event_and_style/EventCardDetails.js
@@ -4,37 +4,53 @@ import CommentsList from "../Comments/CommentsList";
 import ToDos from "../To-do's/ToDos";
 import InviteFriends from "../Invite Friends/InviteFriends";
 import { useEffect, useState } from "react";
+import "@formatjs/intl-datetimeformat/locale-data/en";
 
-
-const EventCardDetails = ({ event, user, setShowDetails}) => {
-
-  const [commentResults, setCommentResults] = useState([])
+const EventCardDetails = ({ event, user, setShowDetails }) => {
+  const [commentResults, setCommentResults] = useState([]);
 
   useEffect(() => {
-      fetch('/comments')
-      .then(resp => resp.json())
-      .then(data => setCommentResults(data))
-  }, [])
+    fetch("/comments")
+      .then((resp) => resp.json())
+      .then((data) => setCommentResults(data));
+  }, []);
 
-  console.log(commentResults)
+  console.log(commentResults);
+
+  let eventDate = new Date(event.date);
+
+  let fmt = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+  });
 
   return (
     <div className="event-card-details">
-        <button onClick={() => setShowDetails(false)}>X</button>
-        <h1 className="event-card-details-title">{event.title}</h1>
-        <h1 className="event-card-details-description">
-          Description: {event.description}
-        </h1>
-        <h1 className="event-card-details-date">Date: {event.date}</h1>
-        <h1 className="event-card-details-time">
-          Time: {event.start_time}-{event.end_time}
-        </h1>
+      <button onClick={() => setShowDetails(false)}>X</button>
+      <h1 className="event-card-details-title">{event.title}</h1>
+      <h1 className="event-card-details-description">
+        Description: {event.description}
+      </h1>
+      {/* <h1 className="event-card-details-date">Date: {event.date}</h1> */}
+      <h1 className="event-card-details-date">
+        Date: {`${fmt.format(eventDate)}`}
+      </h1>
 
-        <InviteFriends /> 
-        <Budget />
-        <ToDos />
-        <CommentsList commentResults={commentResults} />
-        <AddCommentForm commentResults={commentResults} setCommentResults={setCommentResults} user={user} event={event}/>
+      <h1 className="event-card-details-time">
+        Time: {event.start_time}-{event.end_time}
+      </h1>
+      <InviteFriends />
+      <Budget />
+      <ToDos />
+      <CommentsList commentResults={commentResults} />
+      <AddCommentForm
+        commentResults={commentResults}
+        setCommentResults={setCommentResults}
+        user={user}
+        event={event}
+      />
     </div>
   );
 };

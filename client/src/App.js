49f180a8016bffca6e001ab36a1ffd225b22c3b3
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-11-17 10:49:52 -0800

    game code imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import GameOne from './components/GameOne';
--- client/src/components/GameThree.js
+++ client/src/components/GameThree.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import Reacteroids from './reacteroids/Reacteroids';
 
 function GameThree() {
     return(
-        <div>
-            hi
-        </div>
+        <Reacteroids/>
     )
 }
 

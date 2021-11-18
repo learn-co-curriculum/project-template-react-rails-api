cb903e20ca2e36e2bee3942e67ab289540abf27e
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-11-18 08:53:16 -0800

    moved game three CS files to a separate .css

--- client/src/components/GameThree.js
+++ client/src/components/GameThree.js
@@ -2,6 +2,7 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 import Reacteroids from './reacteroids/Reacteroids';
 
+
 function GameThree() {
     return(
         <Reacteroids/>

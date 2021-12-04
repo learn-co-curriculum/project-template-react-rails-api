db5845fc4a42a203504213d02a8d065c55a71f68
Joshua Andes <andesjoshua@comcast.net>
2021-12-04 12:41:56 -0500

    testing commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Header from './components/Header';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Header

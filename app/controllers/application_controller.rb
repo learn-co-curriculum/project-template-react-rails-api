51c187f012f4c4ac60daad8e4cf16b74cc1941dc
Joshua Andes <andesjoshua@comcast.net>
2021-12-06 13:34:38 -0500

    login/logout/signup func, not complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end
+
+  
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :avatar
+  attributes :id, :username, :password, :avatar
 end

73b524d66a4aebfdeef90768dcc88bed55d75c65
Alex Wentz <wentz.alex@gmail.com>
2021-12-06 10:49:14 -0500

    forgot user password

--- db/migrate/20211206153454_create_users.rb
+++ db/migrate/20211206153454_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
+      t.string :password_digest
       t.avatar :belongs_to
 
       t.timestamps

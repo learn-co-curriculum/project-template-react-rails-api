3f5530fe32043f02e320d8924335e2303a7664ee
Shannon Sorensen <sorensen.sms@gmail.com>
2021-12-08 15:57:23 -0600

    added VictoryPage

---
+++ client/src/components/VictoryPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+import React from 'react'
+
+export default function VictoryPage() {
+    return (
+        <div>
+            <h1>Congratulations!</h1>
+            <h2>You made it to the top of Mount Algo!</h2>
+            <h4>Look at that view, isn't it wild?</h4>
+        </div>
+    )
+}

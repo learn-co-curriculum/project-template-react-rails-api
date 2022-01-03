f188b1a81da95c59c5fbbe0b55fe0327dce04e97
webster <wbian618@gmail.com>
2022-01-03 10:10:10 -0500

    Webster test

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Webster Test
         </p>
         <a
           className="App-link"

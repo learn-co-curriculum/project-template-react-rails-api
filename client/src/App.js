7e23206d31a8c05d18df04330d3c6fb19f9f4e92
Bennett <bjupfield@gmail.com>
2022-01-26 16:30:22 -0600

    plz

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import CreateSale from './Components/CreateSale';
-import { Route, Router, Link } from 'react-router-dom';
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>HELP ME</div>
-        <Router>
-          <Route path={"/"} element = {<div>DOasdasasdasddsfadsasOO IT</div>}></Route>
-          <Route path="/sale" element = {CreateSale}>
-          </Route>
-        </Router>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element = {<div>DOasdasasdasddsfadsasOO IT</div>}></Route>
+            <Route path="/sale" element = {CreateSale}>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );

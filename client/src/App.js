c8a06ffc309fd8af03113fd444e2bbfb66894b39
seenso <>
2022-01-31 07:37:00 -0900

    Added a Pet and Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Color Palatte
+// Text - Dark Grey #424242
+// Primary Color - E
+// Secondary Color - #
+
+
+// Petfinder API Key: K7wpPYjZCTiRxVfWsCeKixCdj9iqZ2IKVHTuLbmeWA1XmTBB0M
+// Petfinder Secret: t2MHR3Cw0WlithMtbyOfwN7aRv8c2iBWeApgTbWw
+// Petfinder uses OAuth for secure authentication
+
 function App() {
   return (
     <div className="App">

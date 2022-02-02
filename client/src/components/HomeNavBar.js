28868048051d9fd7a18354304b3baf6f94264bb7
seenso <>
2022-02-02 09:57:52 -0900

    centered navbar and made it responsive

--- client/src/components/HomeNavBar.js
+++ client/src/components/HomeNavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export default function HomeNavBar() {
   return (
-    <div id="home_navbar">
+    <div id="home_navbar" className="navbar">
       <img className="logo" src="./images/logo.png" alt="logo"/>
       THIS IS THE HOME NAV BAR COMPONENT
     </div>

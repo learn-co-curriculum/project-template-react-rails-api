6ba26d206eede5c4630a2391440a8814299a12af
seenso <>
2022-02-02 11:34:22 -0900

    cards rendering. need to work on layout...

--- client/src/components/Pet.js
+++ client/src/components/Pet.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export default function Pet() {
+export default function Pet({ pet }) {
   return (
-    <div id="pet">THIS IS THE PET COMPONENT</div>
+    <div id="pet">
+
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{pet.name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    
+    
+    
+    </div>
   )
 }

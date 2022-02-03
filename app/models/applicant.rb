80a09e2389adb6c8c8cf0fc57345e0e64aabcbf6
seenso <>
2022-02-03 12:09:57 -0900

    Added more validation for applicant model

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,7 +2,16 @@ class Applicant < ApplicationRecord
   has_many :pet_application, dependent: :destroy
   has_many :meetup, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :dob, presence: true
+  validates :phone, presence: true
+  validates :rent_own, presence: true
+  validates :home_type, presence: true
+  validates :length_address, presence: true
+  validates :yard_description, presence: true
+  validates :children, presence: true
+  validates :pet_allergy, presence: true
+  validates :lifestyle, presence: true
 end

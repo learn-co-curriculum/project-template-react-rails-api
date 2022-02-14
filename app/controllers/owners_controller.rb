7dc781f8aa0721740f28709b358b6979653ce2a3
Hannah McCullough <hmac091@gmail.com>
2022-02-14 11:26:50 -0700

    added show methods to stay logged in

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,6 @@
 class OwnersController < ApplicationController
+    before_action :authorize
+    skip_before_action :authorize, only: [:create]
 
     def create
         new_owner = Owner.create!(owner_params)
@@ -9,7 +11,13 @@ class OwnersController < ApplicationController
         owner = Owner.find(params[:id])
         owner.destroy
     end
-
+    
+    # stay logged in 
+    def show
+        owner = Owner.find(session[:owner_id])
+        render json: owner
+    end
+    
     private
 
     def owner_params

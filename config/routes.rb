0d35dacc67c456ed088d79548724aa7d3bc3deb9
Hannah McCullough <hmac091@gmail.com>
2022-02-14 10:35:20 -0700

    task requests and sessions created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   
   resources :services
-  resources :tasks
+  resources :tasks, only: [:create, :update, :destroy]
   resources :bookings
   resources :provider_services
   resources :owners
   resources :providers
   resources :properties
   resources :users
+
+  post "/login", to: "sessions#create"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

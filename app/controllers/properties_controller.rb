5bfcbae170ed7e49b9d101da30a2fba3aa2527ce
Grant Pierce <glpierce174@gmail.com>
2022-02-14 09:01:41 -0800

    Created show & create routes in properties_controller. Added private strong params method t0o properties_controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,23 @@
 class PropertiesController < ApplicationController
+
+    def show 
+        selected_property = Property.find_by(id: params[:id])
+        if selected_property
+            render json: selected_property
+        else
+            render json: { error: "Property not found" }, status: :not_found
+        end
+    end
+
+    def create
+        new_property = Property.create!(property_params)
+        render json: new_property, status: :created
+    end
+
+    private
+
+    def property_params
+        params.permit(:address, :city, :owner_occupied, :owner_id)
+    end
+
 end

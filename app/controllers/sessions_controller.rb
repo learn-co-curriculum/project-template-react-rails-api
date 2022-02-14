988448e85de57ccafeb745afaf10b63df8eb5edf
Hannah McCullough <hmac091@gmail.com>
2022-02-14 10:59:21 -0700

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
        if (Owner.find_by(email: params[:email]))
             owner = Owner.find_by(email: params[:email]
             session[:owner_id] = owner.id
+            render json: owner
         elsif (Provider.find_by(email: params[:email])
             provider = Provider.find_by(email: params[:email]
             session[:provider_id] = provider.id
+            render json: provider
        end
     end
-    
+
+    def destroy
+        session.delete { :owner_id ? :owner_id : :provider_id }
+        head :no_content
+    end
+
 end

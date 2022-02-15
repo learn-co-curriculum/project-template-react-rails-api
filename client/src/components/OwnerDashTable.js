a886754df8361619a2fe5a91740cf339d99ca3f6
Hannah McCullough <hmac091@gmail.com>
2022-02-15 10:34:44 -0700

    owner dash components

---
+++ client/src/components/OwnerDashTable.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function OwnerDashTable() {
+
+    // Table items: Properties, onClick -> display property details 
+
+    return (
+        <div>
+            OwnerDashTable
+        </div>
+    )
+}
+
+export default OwnerDashTable;

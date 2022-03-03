3c40c5d31e2d5ed5d8f2a26987673355e455f983
J Lee <jay.j.lee.1@gmail.com>
2022-03-03 12:46:50 -0500

    seeded and controllers updated. need to test for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  
+private
+  def render_unprocessable_entity(invalid)
+      render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 
+  def render_not_found(error)
+      render json: {error: "#{error.model} Not Found"}, status: :not_found
+  end
 end

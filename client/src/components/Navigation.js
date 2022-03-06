0494e827fc8ce2d250e342044896b48b7c86a4bb
J Lee <jay.j.lee.1@gmail.com>
2022-03-06 11:41:42 -0500

    navigation up and running

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,51 @@
-import {Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
 function Navigation({user, isAuthenticated, setIsAuthenticated, setUser}) {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+   
     function logout() {
         fetch('/logout',{
             method:'DELETE'
@@ -10,18 +55,31 @@ function Navigation({user, isAuthenticated, setIsAuthenticated, setUser}) {
             setUser(null)
         })
     }
-    return (
-        <> 
-   
-         <div>
-          {user?<h1 onClick={logout}> Logout</h1>:<h1><Link to="/login"> Login</Link></h1>}
-           <h1><Link to="/sign_up"> Sign Up</Link></h1>
-           <h1><Link to="/"> Home</Link></h1>
-           {isAuthenticated ? <p>yes</p> : <p>no</p>}
-         </div>
 
-        </>
-    )
+    return (
+        <Box sx={{ width: '100%' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="Home" {...a11yProps(0)} component={Link} to='/' />
+              <Tab label="Sign Up" {...a11yProps(1)} component={Link} to='/sign_up' />
+              {/* <Tab label="Login" {...a11yProps(2)} component={Link} to='/login' /> */}
+              {user?<Tab onClick={logout} label="Logout" {...a11yProps(2)} />:<Tab label="Login" {...a11yProps(3)} component={Link} to='/login' />}
+            </Tabs>
+          </Box>
+        </Box>
+      );
+    // return (
+    //     <> 
+   
+    //      <div>
+    //       {user?<h1 onClick={logout}> Logout</h1>:<h1><Link to="/login"> Login</Link></h1>}
+    //        <h1><Link to="/sign_up"> Sign Up</Link></h1>
+    //        <h1><Link to="/"> Home</Link></h1>
+    //        {isAuthenticated ? <p>yes</p> : <p>no</p>}
+    //      </div>
+
+    //     </>
+    // )
 }
 
 export default Navigation;

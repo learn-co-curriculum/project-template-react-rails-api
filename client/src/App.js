162d1810bd81bd0d3971712c5d709f80598b78eb
J Lee <jay.j.lee.1@gmail.com>
2022-03-05 20:35:01 -0500

    front end and back end linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [userData, setUserData] = useState([])
+  const [bgData, setBGData] = useState([])
+useEffect(() => {
+  fetch("/boardgames")
+  .then((r)=>r.json())
+  // .then((data)=>console.log(data))
+  .then((data)=>setBGData(data))
+}, [])
+console.log("User:")
+useEffect(() => {
+  fetch("/users")
+  .then((r)=>r.json())
+  // .then((data)=>console.log(data))
+  .then((data)=>setUserData(data))
+}, [])
+console.log("Boardgame Data:")
+console.log(bgData)
+console.log("User:")
+console.log(userData)
+
   return (
+
+    
+
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,6 +42,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
       </header>
     </div>

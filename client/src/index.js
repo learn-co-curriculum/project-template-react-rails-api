36a0bb6099ab5bf30f19cba41b3c9bb8bba30fab
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-28 10:06:51 -0400

    updated import address for App.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>

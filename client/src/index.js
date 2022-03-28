5a9b9798f36b16a7f875be82eeba9cb6f57e6890
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-28 10:03:36 -0400

    updated css import address

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>

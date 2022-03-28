8b4d74d0210acb6b6496b9d6263d1cd45282733a
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-28 15:05:15 -0400

    login functionality WIP

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,29 +2,53 @@ import React, {useState} from "react";
 
 function Login() {
   
-const [user, setUser] = useState("")  
-const [password, setPassword] = useState("")
-  
+  const [user, setUser] = useState("")  
+  const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState(null)
+    
+  function handleLogin (e) {
+    e.preventDefault()
+    
+    fetch ('/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({email: user, password: password})
+    })
+      .then(r => r.json())
+      .then(data => {
+        if (data.error) {setErrors(data.error)}
+        else {
+          setErrors(null)
+          console.log("Login Success")
+        }
+      }) 
+  }
   
   return (
     <div>
       <h1>Login</h1>
       <div className="login-container">
         <div className="login-box">
-          <form >
+          <form onSubmit={handleLogin}>
             <div className="login-input-div">
             <label>
                 <input
+                  onChange={(e) => {setUser(e.target.value)}}
+                  placeholder="Enter username"
                   type="text"
                   className="login-input"
+                  value = {user}
                 />
               </label>
             </div>
             <div className="login-input-div">
               <label>
                 <input
+                  onChange={(e) => {setPassword(e.target.value)}}
+                  placeholder="Enter password"
                   type="password"
                   className="login-input"
+                  value = {password}
                 />
               </label>
             </div>
@@ -34,8 +58,10 @@ const [password, setPassword] = useState("")
                 className="button"
               >
                 Log in
-              </button>
+              </button> 
+              <p className="error">{errors ? errors : null}</p>
             </div>
+            
           </form>
         </div>
       </div>

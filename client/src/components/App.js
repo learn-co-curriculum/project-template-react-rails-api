3468d058a586a94fb35884a5f6d953c25c6212eb
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-28 12:20:25 -0400

    added login page and some styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,34 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom"
+import Login from './Login'
 import logo from '../logo.svg';
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+    </Switch>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import {BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 

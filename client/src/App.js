2f6c37960a2774ac1972cbd80b85a01a6dd2b3ad
David <Davidmaxsands@gmail.com>
2022-03-28 09:34:00 -0600

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import HomePage from './Components/Homepage/HomePage'
 import ProfilePage from './Components/Profile/ProfilePage'
 import ProjectPage from './Components/Project/ProjectPage'
 import { Routes, Route } from "react-router-dom"
+import Navbar from './Components/Navbar';
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>TWIDDLE WAKKA</h1>
       <Routes>
         <Route exact path="/" element={<PreLogin />} />
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+import { FaHome } from 'react-icons/fa'
+import { MdNotificationsActive } from 'react-icons/md'
+import { BsSearch } from 'react-icons/bs'
+import logo from '../Twiddle-Wakka.png'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header>
+      <section>
+        <img src={logo} alt='logo' id='logo'/>
+        <div id="search-container">
+            <form id="search-form">
+                <label for="search-bar"></label>
+                <BsSearch id='search-icon'/>
+                <input type="text" id="search-bar" placeholder="Search posts..." />
+            </form>
+        </div>
+        <nav>
+          <ul className='nav-links'>
+            <li className='nav-effect'><NavLink to="/homepage">Home</NavLink></li>
+            <li className='nav-effect'><NavLink to="/profilepage">My Profile</NavLink></li>
+            <li><MdNotificationsActive className='notification'/></li>
+            <li><button className='logout-button'>Logout</button></li>
+          </ul>
+        </nav>
+      </section>
+    </header>
   )
 }
 

df3bea21ce9adb07bb61d8f85eb6af1105a27953
Ethan Isola <isolaethan@gmail.com>
2022-04-12 21:19:45 -0500

    working enemy response and win/lose conditions

--- client/src/components/gameComponents/Fight.js
+++ client/src/components/gameComponents/Fight.js
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
 
 function Fight({charHealth, setCharHealth, level}){
+    const [acted, setActed] = useState(false)
     const [enemy, setEnemy] = useState([])
     const [display, setDisplay] = useState(null)
     const [enHealth, setEnHealth] = useState(100)
 
+
+    //deals damage to enemy based on character attack
+    function dealDamage(){
+        setEnHealth(enHealth-10+enemy.defense)
+        if(enHealth-10+enemy.defense <= 0){
+            console.log("You Win")
+        }
+        setTimeout(()=>{takeDamage()}, 2000)
+    }
+    //Deals damage to character based on enemy attack
+    function takeDamage(){
+        setCharHealth(charHealth-enemy.attack)
+        if(charHealth-enemy.attack <= 0){
+            console.log("You Lose")
+        }
+    }
+    //gets the data for the enemy when the fight starts
     useEffect(()=>{
         fetch(`/enemy/?level=${level}`)
         .then(r=>r.json())
         .then((r)=>setEnemy(r[0]))
         console.log(enemy)
     },[])
-
-    function dealDamage(){
-        setEnHealth(enHealth-10)
-        if(enHealth <= 0){
-            console.log("You Win")
-        }
-    }
-
-    function takeDamage(){
-        setCharHealth(charHealth-10)
-        if(charHealth <= 0){
-            console.log("You Lose")
-        }
-    }
-
+    //updates the display when the health values are updated
     useEffect(()=>{
         setDisplay(
             <div>

66e56ce4f30707a25d9eec8b0cd88b170cbc804a
Felipa Mendez <felipalouise@gmail.com>
2022-04-18 12:47:57 -0500

    removed namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :user_stocks
-  resources :companies, only: [:index, :show, :create]
+ 
+    resources :user_stocks
+    resources :companies, only: [:index, :show, :create]
+    resources :users
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#login"
+    delete "/logout", to: "sessions#logout"
+    get "/current_user", to: "users#show"
 
-  resources :users
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#login"
-  delete "/logout", to: "sessions#logout"
-  get "/current_user", to: "users#show"
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!

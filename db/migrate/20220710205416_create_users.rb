51cdbd8d83895fb08e7d0ba17a0ff9eb20a5e122
Corey Schadeck <corey@schadeck.com>
2022-07-10 18:53:47 -0400

    deleted unneeded table and updated user table.

--- db/migrate/20220710205416_create_users.rb
+++ db/migrate/20220710205416_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :user_name
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
--- db/migrate/20220710212128_create_sessions.rb
+++
@@ -1,8 +0,0 @@
-class CreateSessions < ActiveRecord::Migration[6.1]
-  def change
-    create_table :sessions do |t|
-
-      t.timestamps
-    end
-  end
-end

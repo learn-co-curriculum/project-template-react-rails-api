92b47bd754ab3a02e7724629cf5e017ca030818f
Eli Craig <elidcraig@gmail.com>
2022-07-11 10:27:14 -0700

    validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_many :posted_items, class_name: "Item", dependent: :destroy
 
   has_secure_password
+
+  validates :username, :email, presence: true, uniqueness: true
 end

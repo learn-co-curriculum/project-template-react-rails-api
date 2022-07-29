c851f6ed3515ed3f1805bc3fcf32f4d4848e025b
Nicola Milne <nicolalouisemilne@gmail.com>
2022-07-29 15:44:38 -0600

    fixed routing to push to resources page for the appropriate rec center and to use params to understand which rec center we need to fetch resources for

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,12 @@
 import Header from "./everyone/Header"
 import InformationContainer from "./InformationContainer";
 import "../App.css";
-import { useState, useEffect} from "react"
 
 function App() {
-
-const [recCenters, setRecCenters] = useState([])
-
-useEffect(() => {
-  fetch(`http://localhost:3000/rec_centers`)
-      .then(res => res.json())
-      .then((data) => setRecCenters(data))
-    }, [])
-
   return (
     <div className="App">
       <Header />
-      <InformationContainer recCenters={recCenters}/>
+      <InformationContainer/>
     </div>
   );
 }
--- client/src/components/InformationContainer.js
+++ client/src/components/InformationContainer.js
@@ -2,11 +2,16 @@ import { Route, Switch } from "react-router-dom";
 import AdminContainer from "./admin/AdminContainer";
 import PlayerContainer from "./player/PlayerContainer";
 import EveryoneContainer from "./everyone/EveryoneContainer";
+import { useState, useEffect} from "react"
 
+function InformationContainer() {
+  const [recCenters, setRecCenters] = useState([])
 
-function InformationContainer({recCenters}) {
-
-
+  useEffect(() => {
+    fetch(`http://localhost:3000/rec_centers`)
+        .then(res => res.json())
+        .then((data) => setRecCenters(data))
+      }, [])
 
   return (
     <Switch>

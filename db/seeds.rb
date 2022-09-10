2e59cc4db69e254740ffed26c3b3eb3f2c91e7d9
Connor <connor.cs@protonmail.com>
2022-09-10 15:13:57 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'pry'
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington','West Virginia', 'Wisconsin', 'Wyoming']
+
+STATES.each do |state|
+    State.find_or_create_by(name: state)
+end
+
+State.all.each do |state|
+    url = URI("https://trailapi-trailapi.p.rapidapi.com/activity/?limit=25&q-state_cont=#{state.name}&q-activities_activity_type_name_eq=hiking")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = '68401ebe54msh8af2606513d710bp1b21b0jsn49f916645636'
+    request["X-RapidAPI-Host"] = 'trailapi-trailapi.p.rapidapi.com'
+    response = http.request(request)
+    ap response.read_body
+    json = JSON.parse response.body
+
+    json.each do |x, y|
+         trail = Trail.find_or_create_by(
+            name: y["name"],
+            city: y["city"],
+            state_id: state.id,
+            description: y["description"],
+            directions: y["directions"],
+            latitude: y["lat"].to_f,
+            longitude: y["lon"].to_f,
+            length: y["activities"]["hiking"]["length"]
+         )
+    
+    end
+
+end
+
+
+
+
+binding.pry

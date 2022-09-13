712af74e76029d65599662c0ca347e722cc5ead7
Connor <connor.cs@protonmail.com>
2022-09-13 10:45:05 -0400

    added login form

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useState, useHistory } from "react";
 
+//useHistory hook to redirect user to explore after logging in
 export default function Login() {
+  
+  const [formData, setformData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const history = useHistory()
+  const {name, email, password} = formData
+
+  function handleChange(e) {
+    setformData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+  
+
+  //double check this
+  function handleSubmit() {
+    e.preventDefault()
+    user = {
+      name, email, password
+    }
+    fetch('/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(user)
+    })
+    .then(res=> res.json())
+    .then(history.push('/explore'))
+  }
+
+
+
   return (
-    <main className='login-page'>
-    <div>Login form goes here</div>
+    <main className="login-page">
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input type="text" name="name" value={name} onChange={handleChange}/>
+        <label>Email</label>
+        <input type="text" name="email" value={email} onChange={handleChange}/>
+        <label>Password</label>
+        <input type="password" name="password" value={password} onChange={handleChange}/>
+      </form>
     </main>
-  )
+  );
 }

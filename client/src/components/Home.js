48216600c2ce3edcd29ceaf2e7f5f4e20885050d
Brennan Davis <brennantd@gmail.com>
2022-09-13 10:55:33 -0600

    everything is done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import fest1 from '../assets/fest-3.jpeg'
+import fest2 from '../assets/fest-1.jpeg'
+import fest3 from '../assets/fest-2.jpeg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div id="dcl-intro">
+        <h1>Denver City Limits: you'll never go to another festival.</h1>
+      </div>
+      <h4>March 25, 2023</h4>
+      <div id='dcl-desc-container'>
+        <div id='fest1-container'>
+          <img src={fest1} alt="festival"></img>
+        </div>
+        <div id='dcl-desc'>
+          <p>
+            Dissatisfied by modern music festivals, festival junkies Thing1 and Thing2 decided to take matters into their own hands. After doing “extensive” research, traveling across the world to the hottest and trendiest festivals, your festive hosts have compiled a once-in-a-lifetime musical experience.
+          </p>
+        </div>
+      </div>
+      <div id="dcl-date-location">
+        <h1>Friday, March 25, 2023</h1>
+        <h1>-</h1>
+        <h1>Denver-Washington Park</h1>
+      </div>
+      <div id='dcl-desc-2-container'>
+        <div id='dcl-desc-2'>
+          <p>
+            DCL puts the music fan first. Our lineup is always source-crowded. Bands never turn down this event.
+          </p>
+        </div>
+        <div id='fest-2-container'>
+          <img src={fest2} alt="festival-2"></img>
+        </div>
+        <div id='featured-artists'>
+          <h1>This is where we can fetch, then iterate over some "featured" artists to display their names. Shows that our display can be dynamic with the data.</h1>
+        </div>
+        <div id='dcl-desc-3-container'>
+          <div id='fest-3-container'>
+            <img src={fest3} alt='festival-3'></img>
+          </div>
+          <div id='dcl-desc-3'>
+            <p>DCL provides you with the most diverse group of musical artists. From EDM, to rock, to jazz, and even the advant-garde "not music", you can be assured your niche musical addiction gets fufilled.</p>
+          </div>
+        </div>
+        <div id='tickets-container'>
+          <h1><Link className="route-link" to="/tickets">Tickets</Link></h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,69 +1,69 @@
 import React, { useState } from 'react'
 
-function Signup({setCurrentUser}) {
+function Signup({ setCurrentUser }) {
 
 
 
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
 
-    const { name, email, password } = formData
+    const { name, email, password } = formData
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setFormData({ ...formData, [name]: value })
-        console.log(formData)
-    }
+    function handleChange(e) {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+        console.log(formData)
+    }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        fetch("http://localhost:3000/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData)
-        })
-            .then((res) => {
-                if (res.ok) {
-                    res.json().then((formData) => {
-                        setCurrentUser(formData)
-                        alert(`Hello ${formData.name}`)
-                    });
-                } else {
-                    res.json().then((errors) => {
-                        console.error(errors);
-                    });
-                }
-            });
-    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("http://localhost:3000/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+            .then((res) => {
+                if (res.ok) {
+                    res.json().then((formData) => {
+                        setCurrentUser(formData)
+                        alert(`Hello ${formData.name}`)
+                    });
+                } else {
+                    res.json().then((errors) => {
+                        console.error(errors);
+                    });
+                }
+            });
+    }
 
-    return (
-        <div>
-            <form id="sign-up" onSubmit={handleSubmit}>
-                <label>
-                    Username
-                </label>
-                <input type='text' name='name' value={name} onChange={handleChange}></input>
+    return (
+        <div>
+            <form id="sign-up" onSubmit={handleSubmit}>
+                <label>
+                    Username
+                </label>
+                <input type='text' name='name' value={name} onChange={handleChange}></input>
 
-                <label>
-                    Email
-                </label>
-                <input type='text' name='email' value={email} onChange={handleChange} />
+                <label>
+                    Email
+                </label>
+                <input type='text' name='email' value={email} onChange={handleChange} />
 
-                <label>
-                    Password
-                </label>
-                <input type='password' name='password' value={password} onChange={handleChange} />
+                <label>
+                    Password
+                </label>
+                <input type='password' name='password' value={password} onChange={handleChange} />
 
 
-                <input type='submit' value='Sign up!' />
-            </form>
-        </div>
-    )
+                <input type='submit' value='Sign up!' />
+            </form>
+        </div>
+    )
 }
 
 export default Signup

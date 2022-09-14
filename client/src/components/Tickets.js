f6f2f8850d79696a82c2ae8612b5013a9963eb55
Noah Licata <noahinbldr@gmail.com>
2022-09-14 09:43:40 -0600

    added some more ticket stuff

--- client/src/components/Tickets.js
+++ client/src/components/Tickets.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function Tickets() {
+  const [tier, setTier] = useState(true);
   const [ticketPriceGA, setTicketPriceGA] = useState("$0.00");
   const [ticketPriceVIP, setTicketPriceVIP] = useState("$0.00");
 
@@ -13,55 +14,81 @@ function Tickets() {
     setTicketPriceVIP(`$${e.target.value * 500}.00`);
   };
 
+  let toggleTier = () => {
+    setTier(!tier);
+  };
+
   return (
     <div id="container">
       <div id="title-container">
         <div id="titles">Tickets</div>
       </div>
 
-      <div id="card">
-        <div id="type-container">
-          <button id="type">GA</button>
+      {tier ? (
+        <div id="card">
+          <div id="type-container"></div>
+          <h1 class="ticket-title">General Admission</h1>
+          <h1 class="ticket-price">$250.00</h1>
+          <div id="type-container"></div>
+          <button id="type" onClick={toggleTier}>
+            Change Tier
+          </button>
+          <p id="description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
+            suscipit eaque? Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+            deserunt nobis suscipit eaque? Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Molestias aut, repellat ipsum facere
+            voluptate dicta obcaecati deserunt nobis suscipit eaque? Lorem,
+            ipsum dolor sit amet consectetur adipisicing elit. Molestias aut,
+            repellat ipsum facere voluptate dicta obcaecati deserunt nobis
+            suscipit eaque?
+          </p>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            placeholder="How Many?"
+            onChange={handleChangeGA}
+            min="0"
+          ></input>
+          <p>Total: {ticketPriceGA}</p>
+          <button id="cart-button">Add to Cart</button>
         </div>
-        <h1 class="ticket-title">General Admission</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
-          suscipit eaque?
-        </p>
-        <input
-          type="number"
-          id="quantity"
-          name="quantity"
-          placeholder="How Many?"
-          onChange={handleChangeGA}
-          min="0"
-        ></input>
-        <p>{ticketPriceGA}</p>
-        <button id="cart-button">Add to Cart</button>
-      </div>
-
-      <div id="card">
-        <div id="type-container">
-          <button id="type">VIP</button>
+      ) : (
+        <div id="card">
+          <div id="type-container"></div>
+          <h1 class="ticket-title">VIP</h1>
+          <h1 class="ticket-price">$500.00</h1>
+          <div id="type-container"></div>
+          <button id="type" onClick={toggleTier}>
+            Change Tier
+          </button>
+          <p id="description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
+            suscipit eaque? Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+            deserunt nobis suscipit eaque? Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Molestias aut, repellat ipsum facere
+            voluptate dicta obcaecati deserunt nobis suscipit eaque? Lorem,
+            ipsum dolor sit amet consectetur adipisicing elit. Molestias aut,
+            repellat ipsum facere voluptate dicta obcaecati deserunt nobis
+            suscipit eaque?
+          </p>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            placeholder="How Many?"
+            onChange={handleChangeVIP}
+            min="0"
+          ></input>
+          <p>Total: {ticketPriceVIP}</p>
+          <button id="cart-button">Add to Cart</button>
         </div>
-        <h1 class="ticket-title">VIP</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
-          suscipit eaque?
-        </p>
-        <input
-          type="number"
-          id="quantity"
-          name="quantity"
-          placeholder="How Many?"
-          onChange={handleChangeVIP}
-          min="0"
-        ></input>
-        <p>Total: {ticketPriceVIP}</p>
-        <button id="cart-button">Add to Cart</button>
-      </div>
+      )}
     </div>
   );
 }

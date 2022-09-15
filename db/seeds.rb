bbb081c3aee7efade12802b87a2c85d563a09eb5
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-15 07:57:31 -0600

    starting seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,4 +53,397 @@ end
     )
 end
 
+# Artist.create(
+#     name: "J. Cole" ,
+#     bio: 'Jermaine Lamarr Cole is an American rapper, singer, songwriter, and record producer. He is regarded as one of the most influential rappers of his generation.',
+#     genre: 'Hip-Hop',
+#     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNQ7SQ40l_zbDoX5UPYDbJ3lIkyw_8CSsYyw&usqp=CAU",
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: "Rockies",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Kaskade' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Zeds Dead' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Ganja White Night' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'A$AP Rocky' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Mike Jones' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Bad Bunny' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Calvin Harris' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Deorro' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Disclosure' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Dom Dolla' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Flume' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Jauz' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Snoop Dogg' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Tame Impala' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'JoJi' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Kayzo' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Kid Cudi' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Lil Wayne' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Miguel' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Oliver Heldens' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'Post Malone' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Red Hot Chili Peppers' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'ScHoolboy Q' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'Skrillex' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: 'The Weeknd' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: 'ZHU' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+# Artist.create(
+#     name: '' ,
+#     bio: ''
+#     genre: '',
+#     image: ''
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1,)
+# Artist.create(
+#     name: '' ,
+#     bio: '',
+#     genre: '',
+#     image: '',
+#     performance_time: "#{rand(12..22)}:30:00",
+#     stage: " ",
+#     festival_id: 1)
+
 puts "👽 Done Seeding"

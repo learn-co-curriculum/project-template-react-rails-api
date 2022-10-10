34323002eace17a0bc792527f38d39468d9ce3e8
phinic925 <phinicnyangate480@gmail.com>
2022-10-10 15:50:44 +0100

    add blog associations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
 class Blog < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :blogs
     has_many :comments, through: :blogs
+    has_many :categories
 end

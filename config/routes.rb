952577135973db08fb7e1074948483fe86be38fa
Simon kamau <simon.kamau@student.moringaschool.com>
2022-10-11 14:52:57 +0300

    created folder structures for comments model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :comments
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
--- db/migrate/20221011114538_create_users.rb
+++ db/migrate/20221011114538_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
       t.string :username
       t.string :email
       t.string :password

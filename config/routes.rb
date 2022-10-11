abf0871c1f4ee27881aad33d7026717170df9018
phinic925 <phinicnyangate480@gmail.com>
2022-10-11 11:43:26 +0100

    creates blog and comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
-  
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   resources :users
+  resources :blogs
+  resources :comments
 end

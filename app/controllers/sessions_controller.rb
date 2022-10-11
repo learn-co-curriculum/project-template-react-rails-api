de38214b0d1742e49cc09f7118f4a35f3f34b066
phinic925 <phinicnyangate480@gmail.com>
2022-10-11 12:59:58 +0100

    logs out user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,10 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         render json: user
     end 
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+      end
+      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
 end

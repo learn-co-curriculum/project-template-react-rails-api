c784f6dfccb216dcf4440579c0fbee811947cb94
phinic925 <phinicnyangate480@gmail.com>
2022-10-11 12:37:41 +0100

    creat comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def create
+         comments = Comment.create!(comment_params)
+          render json: comments, status: :created
+         
+    end
+    private
+
+      def comment_params
+        params.permit(:description, :blog_id, :user_id)
+      end
+      def co
+          
+      end
+
+      def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def create
+        
+        
+    end
 end

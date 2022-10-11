7a20289644d41026189045d9b353f37e9c88f72b
phinic925 <phinicnyangate480@gmail.com>
2022-10-11 13:29:29 +0100

    updates password protecion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
         session[:user_id] = user.id
-        render json: user
+        render json: user,status: :created
+           
+       else
+        render json: { error: "Invalid username or password"}, status: :unauthorized
+           
+       end
     end 
 
     def destroy

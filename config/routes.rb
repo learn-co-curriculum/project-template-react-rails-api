11a372d2c31b4f8ed4e57620b370d53389abc0f8
Simon kamau <simon.kamau@student.moringaschool.com>
2022-10-11 14:46:41 +0300

    scaffolded user structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

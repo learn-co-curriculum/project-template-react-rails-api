72c6ac20165eeebc1e155b095e8b7bcb863c8907
Simon kamau <simon.kamau@student.moringaschool.com>
2022-10-11 15:06:14 +0300

    updated user structure

---
+++ db/migrate/20221011120356_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end

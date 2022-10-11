a29ced7e90320057c1947bb9d0a8265db008f24b
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-10-11 15:39:01 +0300

    setting up seed data and added Faker gem

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ group :test do
 end
 
 gem "active_model_serializers", "~> 0.10.12"
+
+gem "faker"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+Buyer.destroy_all
+Product.destroy_all
+Review.destroy_all
+Wishlist.destroy_all
+
+puts "🎯 Seeding Admin data..."
+# Admin
+admin = Admin.create(username: "admin", email: "admin@sokoadmin.io")
+
+puts "🎯 Seeding Buyer data..."
+# Buyer
+buyer_1 = Buyer.create(username: "luke", email: "luke@soko.io")
+
+puts "🎯 Seeding Review data..."
+# Review
+review_1 = Review.create(comment: Faker::Lorem.sentence(word_count: 5, supplemental: true), rating: Faker::Number.within(range: 1..5))
+
+puts "🎯 Seeding Wishlist data..."
+# Wishlist
+wishlist_1 = Wishlist.create(product_name: Faker::Commerce.product_name, buyer_id: buyer_1.id)
+
+puts "🎯 Seeding Product data..."
+# Product
+Product.create(product_name: Faker::Commerce.product_name, 
+    product_image_url: Faker::LoremFlickr.image, 
+    product_description: Faker::Lorem.sentence(word_count: 12)
+    quantity: Faker::Number.digit,
+    price: Faker::Commerce.price
+    subscribe: "Yes",
+    admin_id: admin.id,
+    buyer_id: buyer_1.id,
+    review_id: review_1.id,
+    wishlist_id: wishlist_1.id,
+)
+
+puts "🎯 Done seeding data"

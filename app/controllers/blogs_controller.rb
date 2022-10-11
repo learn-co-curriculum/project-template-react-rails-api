af7ffebcd4238d69c31138be29052429fe50407b
phinic925 <phinicnyangate480@gmail.com>
2022-10-11 12:08:25 +0100

    creates new blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,22 @@
 class BlogsController < ApplicationController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
     def index
         blog = Blog.all
         render :json blog, include: :comments
        
+    end
+    def create
+        blog = Blog.create!(blog_params)
+        render :json blog, status: :created
+    end
+
+    private
+    def blog_params
+        params.permit(:user_id, :title, :category, :image_url, :author_id )
     end
     
 end

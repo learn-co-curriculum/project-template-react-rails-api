c613570e45e9871cf437eff104f02f7c817378d2
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-10-11 15:24:01 +0300

    added the components

---
+++ client/src/components/About.js
@@ -0,0 +1,9 @@
+function About() {
+    return (  
+        <div>
+            About
+        </div>
+    );
+}
+ 
+export default About;
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (  
+        <div>
+            Home
+        </div>
+    );
+}
+ 
+export default Home;

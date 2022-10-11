62e3d7a4e9a3e4a41da80a57650acc6e1ac531d1
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-11 16:13:41 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :properties
-  resources :sellers
-  resources :users
+  resources :properties, only: [:index, :show]
+  resources :sellers, only: [:index, :show, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }

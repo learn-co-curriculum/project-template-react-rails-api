3ac5f241c04032d8e467b836a3f7d904ff4c36e8
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-10-12 23:09:55 +0300

    adds categories to controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,48 @@
 class CategoriesController < ApplicationController
-    
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def create
+         categories = Category.create!(category_params)
+          render json: category, status: :created
+         
+    end
+    def update
+        category = Category.find_by(id: params[:id])
+
+        if category
+            category.update(category_params)
+            render json: category
+        else
+            render json: {error: "Category not found"}, status: :not_found
+        end
+        
+    end
+    def destroy
+       
+        category = Category.find_by(id: params[:id])
+        if category
+          category.destroy
+          head :no_content
+        else
+          render json: { error: "category not found" }, status: :not_found
+        end
+      end
+    private
+
+      def category_params
+        params.permit(:name, :blog_id, :user_id)
+      end
+      def category_params
+        params.permit(:name)
+          
+      end
+
+      def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+end
+
     
 end

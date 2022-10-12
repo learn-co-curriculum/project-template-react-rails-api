d12b98ce50ee4e558a8fced96b6f5423c1d1cda0
gitahievans <evanygk@gmail.com>
2022-10-12 20:00:06 +0300

    Added validations in tenant model

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,5 +2,6 @@ class Tenant < ApplicationRecord
     has_many :houses
     has_many :landlords, through: :houses
 
-    
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end

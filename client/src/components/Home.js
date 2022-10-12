5dd2497dbea0288733a0f0f58ddb3f00d5fa4c5e
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-13 01:33:25 +0300

    add a handle click function to the view properties button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import { Carousel, Row, Col, Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Properties from "./properties";
 
 function Home({ properties }) {
   return (
@@ -26,26 +27,7 @@ function Home({ properties }) {
           View Properties
         </Button>{" "}
       </Carousel>
-      <Container>
-        <Row xs={1} md={3} className="g-4">
-          {Array.from({ length: 4 }).map((_, idx) => (
-            <Col>
-            {/*  */}
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Properties properties={properties} />
     </>
   );
 }
---
+++ client/src/components/Property.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Property({ property }) {
+
+  return (
+    <></>
+  );
+}
+
+export default Property;

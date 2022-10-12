aad453be2a7ffec06edd5fec0e8232b796fef8ce
Alex Mwaura <alexmwaura36@gmail.com>
2022-10-13 02:06:29 +0300

    updated  app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
 // import Home from "./components/Home";
 // import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-=======
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Property from "./components/Property";

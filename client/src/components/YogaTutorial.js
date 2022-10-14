fe95e550723902fa3f1dc8575e6562b688a631a2
Milton Ouma <ouma.nyang1@gmail.com>
2022-10-14 09:04:45 +0300

    fetch records

--- client/src/components/YogaTutorial.js
+++ client/src/components/YogaTutorial.js
@@ -19,26 +19,49 @@ const fetchYogaTutorials = async () => {
 }
 if(yogatutorials.length >0){
   return (
-    {yogatutorials.map((tutorial, index) => (
-    <div className="col-md-4 mt-4" key={index} yogatutorials={tutorial}  onDelete={deleteTutorial}>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={tutorial.description} />
-      <Card.Body>
-        <Card.Title>{tutorial.name}
-          <hr></hr>
-  </Card.Title>
-        <Card.Text>
-        {tutorial.description}
-        </Card.Text>
-        <Button className='rounded-0 btn-sm' variant="primary">Rating</Button>
-      </Card.Body>
-    </Card>
-     </div>
-         ))}
-  )
+  //   {yogatutorials.map((tutorial, index) => (
+  //   <div className="col-md-4 mt-4" key={index} yogatutorials={tutorial}  onDelete={deleteTutorial}>
+  //   <Card style={{ width: '18rem' }}>
+  //     <Card.Img variant="top" src={tutorial.description} />
+  //     <Card.Body>
+  //       <Card.Title>{tutorial.name}
+  //         <hr></hr>
+  // </Card.Title>
+  //       <Card.Text>
+  //       {tutorial.description}
+  //       </Card.Text>
+  //       <Button className='rounded-0 btn-sm' variant="primary">Rating</Button>
+  //     </Card.Body>
+  //   </Card>
+  //    </div>
+  //        ))}
+  // )
 
-    }else {
-    'No tutorials available for now'
-    }}
+  //   }else {
+  //   'No tutorials available for now'
+  //   }}
+
+
+
+
+    
+const YogaTutorial = ({ tutorial, onDelete }) => {
+  return (
+  <div className="col-md-4 mt-4" >
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={tutorial.description} />
+  <Card.Body>
+  <Card.Title>{tutorial.name}
+  <hr></hr>
+  </Card.Title>
+  <Card.Text>
+  {tutorial.description}
+  </Card.Text>
+  <Button className='rounded-0 btn-sm' variant="primary">Rating</Button>
+  </Card.Body>
+  </Card>
+  </div>
+)
+}
 
 export default YogaTutorial;
--- client/src/components/YogaTutorials.js
+++ client/src/components/YogaTutorials.js
@@ -3,14 +3,37 @@ import React from 'react';
 // import Card from 'react-bootstrap/Card';
 import YogaTutorial from './YogaTutorial';
 function YogaTutorials (){
-  return (
-    <div className='row'>
-      <YogaTutorial/>
-      <YogaTutorial/>
-      <YogaTutorial/>
-      <YogaTutorial/>
-      <YogaTutorial/>
-      </div>
-  )
+
+  onst [yogatutorials, setYogaTutorails] = useState([])
+  useEffect(() => {
+    const getYogaTutorials = async () => {
+    const YogaTutorials = await fetchYogaTutorials()
+    setYogaTutorails(YogaTutorials)
+    }
+    getYogaTutorials()
+}, [])
+
+const fetchYogaTutorials = async () => {
+  const res = await fetch('/yoga')
+  const data = await res.json()
+  return data
 }
+
+<Route path='/'
+exact
+render={(props) => (
+<>
+{showAddConst && <AddConstituency  onAdd={addConstituency} onAddConst={() => setShowAddConst(!showAddConst)} showAddConst={showAddConst} />}
+
+{constituencies.length > 0 ? (
+{yogatutorials.map((tutorial, index) => (
+        <YogaTutorial key={index} constituency={constituency}   />
+      ))}
+
+) : (
+  'No constituency records found'
+)}
+</>
+
+});
 export default YogaTutorials;

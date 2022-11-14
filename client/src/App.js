d0f1130de3e8c5b0e8467dabd8c3d81865559377
Seo Choi <seochoi56@gmail.com>
2022-11-14 15:17:25 -0500

    initial commit with test api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  // const [showStuff, setStuff] = useState([])
+
+  //TEST CALL TO THE API AND GETTING DATA BACK COMPLETE
+
+  // useEffect(() => {
+  //   fetch('https://wger.de/api/v2/equipment', {
+  //     headers: { 'Content-Type' : 'application/json' },
+  //     method: 'GET'
+  //   })
+  //   .then(res => {
+  //     // console.log(res)
+  //     return res.json()
+  //   })
+  //   .then(data => {
+  //     console.log(data.results)
+  //     setStuff(data.results)
+  //   })
+  // }, [])
+
+  // function showEqipment(item) {
+  //   return (
+  //     <li>{item.name}</li>
+  //   )
+  // }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* {showStuff.map(item => showEqipment(item))} */}
         </p>
         <a
           className="App-link"

860e477b4cacaf1cd8cb5b868f743c08150ac107
Sam Avery <sam.avery1@gmail.com>
2022-11-15 17:31:07 -0600

    Stub progress, commenting out profile in App, changed image to image url in concert container

--- client/src/components/StubContainer.js
+++ client/src/components/StubContainer.js
@@ -4,13 +4,29 @@ import Stubs from './Stubs'
 const StubContainer = ({user}) => {
   const stubs = user.concerts
   function isStub(date){
-    return date < Date.today
+    return Date.parse(date) < Date.now()
   }
   const stubsList = stubs.filter(isStub)
 
+  const myStubsList = stubsList.map((concert) => {
+    return (
+      <Stubs
+      key={concert.id}
+      id={concert.id}
+      date={concert.date}
+      band={concert.band.name}
+      image={concert.band.image}
+      venue_name={concert.venue.name}
+      venue_city={concert.venue.city}
+      comment={concert.comment}
+      />
+    )
+  })
+
   return (
-    <div>{stubsList}</div>
+    <div>{myStubsList}</div>
   )
 }
 
 export default StubContainer
+
--- client/src/components/UpcomingConcerts.js
+++ client/src/components/UpcomingConcerts.js
@@ -2,10 +2,25 @@ import React from 'react'
 import ConcertCard from './ConcertCard'
 
 const UpcomingConcerts = ({user}) => {
-  const myConcerts = user.concerts.map((concert) => {
+  const myConcerts = user.concerts
+  function isUpcoming(date){
+    return date >= Date.today
+  }
+  const myUpcomingConcerts = myConcerts.filter(isUpcoming)
+  
+  const myUpcomingConcertList = myUpcomingConcerts.map((concert) => {
     return (
       <ConcertCard
       key={concert.id}
+      id={concert.id}
+      date={concert.date}
+      band={concert.band.name}
+      image={concert.band.image}
+      venue_name={concert.venue.name}
+      venue_city={concert.venue.city}
+      tickets_remaining={concert.tickets_remaining}
+      ticket_link={concert.link}
+      comment={concert.comment}
       />
       )
   })
@@ -13,7 +28,7 @@ const UpcomingConcerts = ({user}) => {
   return (
     <div>
       <h1>My upcoming concerts</h1>
-      <ul>{myConcerts}</ul>
+      <ul>{myUpcomingConcertList}</ul>
     </div>
   )
 }

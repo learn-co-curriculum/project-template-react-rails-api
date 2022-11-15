61c51592b96f4cf7f0e3642a7c9576169eb3dd41
Nico Garbaccio <garbaccio20@gmail.com>
2022-11-15 12:35:23 -0500

    Search bar component

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,1 +1,17 @@
+import { useState } from 'react';
 
+function SearchBar({ search, setSearch }) {
+    return (
+        <div className='search'>
+            <input
+                type='text'
+                id='search'
+                placeholder='Search by band, venue, or city'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+        </div>
+    )
+}
+
+export default SearchBar;

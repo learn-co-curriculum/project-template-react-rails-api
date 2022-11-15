f44cd3d4aeec62cca7db1995780aef72a0158fa3
Nico Garbaccio <garbaccio20@gmail.com>
2022-11-15 16:50:29 -0500

    Concerts container

--- client/src/components/ConcertCard.js
+++ client/src/components/ConcertCard.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const ConcertCard = () => {
+const ConcertCard = ( {key, id, date, band, image, venue_name, venue_city, tickets_remaining, ticket_link, comment} ) => {
+  
   return (
-    <div>ConcertCard</div>
+    <div>
+      <img src={image} alt={band}></img>
+      <h3>{band}</h3>
+      <h3>{date}</h3>
+      <h4>{venue_name}</h4>
+      <h4>{venue_city}</h4>
+      <h5>{tickets_remaining < 100 ? "Low ticket warning!" : null}</h5>
+      <h5>{tickets_remaining = 0 ? "SOLD OUT" : <button><a href={ticket_link} alt="Tickets">Buy tickets</a></button>}</h5>
+    </div>
   )
 }
 

2c22b549b965ed7bc24a596482b3ddcda22caeee
Sam Avery <sam.avery1@gmail.com>
2022-11-15 17:49:40 -0600

    favorites container

--- client/src/components/FavoritesContainer.js
+++ client/src/components/FavoritesContainer.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const FavoritesContainer = () => {
+const FavoritesContainer = ({genre_1, genre_2, genre_3, favorite_band}) => {
   return (
-    <div>FavoritesContainer</div>
+    <div>
+      <div>
+        <h1>My favorite band</h1>
+        <h3>{favorite_band}</h3>
+        {/* use Martine to let users upload band image? */}
+      </div>
+
+      <div>
+        <h1>My favorite genres</h1>
+        <h3>{genre_1}</h3>
+        <h3>{genre_2}</h3>
+        <h3>{genre_3}</h3>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -30,7 +30,7 @@ const Profile = ({user}) => {
       <h2>Profile</h2>
       <h4>See your concert stubs, your upcoming concerts, and your favorites!</h4>
       <UpcomingConcerts />
-      <FavoritesContainer />
+      <FavoritesContainer genre_1={user.genre_1} genre_2={user.genre_2} genre_3={user.genre_3} favorite_band={user.favorite_band}/>
       <StubContainer />
     </div>
   )

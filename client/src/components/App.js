4bc50adacb1c2748c25e69da27c4786a9bc4577c
Tyler Hall <thall2980@gmail.com>
2022-11-15 11:48:51 -0500

    stuff

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,10 @@
-import logo from '../logo.svg';
 import '../App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [band, setBands] = useEffect([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

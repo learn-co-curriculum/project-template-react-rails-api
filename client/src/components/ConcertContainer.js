7c119ab54420120d65572b434fa0c67e552a9332
Sam Avery <sam.avery1@gmail.com>
2022-11-17 15:05:28 -0600

    Concert formatting and profile button formatting

--- client/src/components/ConcertContainer.js
+++ client/src/components/ConcertContainer.js
@@ -137,20 +137,23 @@ const ConcertContainer = ( {user, bands, concerts, setConcerts, displayedVenues,
   });
 
   // Render all concerts
-  const allConcerts = filteredExploreConcerts.map((concert) => {
+  const allConcerts = concerts.map((concert) => {
     return (
       <ConcertCard
-          key={concert.id}
-          id={concert.id}
-          date={concert.date}
-          band={concert.band.name}
-          image={concert.band.image_url}
-          venue_name={concert.venue.name}
-          venue_city={concert.venue.city}
-          venue_state={concert.venue.state}
-          tickets_remaining={concert.tickets_remaining}
-          ticket_link={concert.ticket_link}
-          comment={concert.comment}
+        key={concert.id}
+        id={concert.id}
+        date={concert.date}
+        band={concert.band.name}
+        band_id={concert.band.id}
+        image={concert.band.image_url}
+        user={user}
+        venue_name={concert.venue.name}
+        venue_id={concert.venue.id}
+        venue_city={concert.venue.city}
+        venue_state={concert.venue.state}
+        tickets_remaining={concert.tickets_remaining}
+        ticket_link={concert.ticket_link}
+        comment={concert.comment}
         />
     )
   })

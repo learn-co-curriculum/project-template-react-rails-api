5cf957f0da00258beda59b73b98e5152deece61d
Tyler Hall <thall2980@gmail.com>
2022-11-17 11:56:02 -0500

    merge success

--- client/src/components/Concerts.js
+++ client/src/components/Concerts.js
@@ -1,9 +1,6 @@
-
-import React from 'react'
-import ConcertContainer from './ConcertContainer'
-import SearchBar  from './SearchBar'
-
-const Concerts = ({bands, user, concerts, setConcerts, venues, setVenues, displayedBands, displayedVenues, search, setSearch }) => {
+import React from "react";
+import ConcertContainer from "./ConcertContainer";
+import SearchBar from "./SearchBar";
 
 const Concerts = ({
   bands,
@@ -12,15 +9,30 @@ const Concerts = ({
   setConcerts,
   venues,
   setVenues,
-  handleNewStub
+  displayedBands,
+  displayedVenues,
+  search,
+  setSearch,
 }) => {
   return (
     <div>
-      <h2 className='page-header'>Concerts</h2>
-    <SearchBar displayedBands={displayedBands} displayedVenues={displayedVenues} search={search} setSearch={setSearch}/>
-    <ConcertContainer user={user} bands={bands} concerts={concerts} setConcerts={setConcerts} venues={venues} setVenues={setVenues} handleNewStub={handleNewStub} />
+      <h2 className="page-header">Concerts</h2>
+      <SearchBar
+        displayedBands={displayedBands}
+        displayedVenues={displayedVenues}
+        search={search}
+        setSearch={setSearch}
+      />
+      <ConcertContainer
+        user={user}
+        bands={bands}
+        concerts={concerts}
+        setConcerts={setConcerts}
+        venues={venues}
+        setVenues={setVenues}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Concerts;

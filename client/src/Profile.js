04d00a62b9dc384a6ff3ee6d62d9d4d26ee7372e
Deryn Brown <Deryn.2017@gmail.com>
2022-11-17 19:08:02 -0600

    profile css

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -24,8 +24,15 @@ function Profile({ user, listOfWorkOuts, setItemToReview, listOfMeals }) {
           <h2>{workout.name}</h2>
           <p>{workout.difficulty}</p>
           <p>{workout.muscle}</p>
-          <button className="border-4 border-red-400 bg-red-400 rounded-md">Delete Work-Out</button>
-          <button className="border-4 border-red-400 bg-red-400 rounded-md" onClick={handleClickReviewShow}>Write Review</button>
+          <button className="border-4 border-red-400 bg-red-400 rounded-md">
+            Delete Work-Out
+          </button>
+          <button
+            className="border-4 border-red-400 bg-red-400 rounded-md"
+            onClick={handleClickReviewShow}
+          >
+            Write Review
+          </button>
           {showReviewTextBox ? <WorkOutReviews /> : null}
         </div>
       );
@@ -38,11 +45,20 @@ function Profile({ user, listOfWorkOuts, setItemToReview, listOfMeals }) {
       console.log(meal);
       return (
         <div>
-        <img src={meal.food.image} />
+          <img src={meal.food.image} />
           <p>{meal.food.label}</p>
           <p>{meal.food.nutrients["ENERC_KCAL"]}</p>
-          <button className="border-4 border-red-400 bg-red-400 rounded-md" >Delete Meals</button>
-          <button className="border-4 border-red-400 bg-red-400 rounded-md" onClick={handleClickReviewShow}>Write Review</button>
+          <div className="p-4">
+            <button className="border-4 border-red-400 bg-red-400 rounded-md">
+              Delete Meals
+            </button>
+            <button
+              className="border-4 border-red-400 bg-red-400 rounded-md"
+              onClick={handleClickReviewShow}
+            >
+              Write Review
+            </button>
+          </div>
           {showReviewTextBox ? <MealsReviews /> : null}
         </div>
       );
@@ -51,20 +67,20 @@ function Profile({ user, listOfWorkOuts, setItemToReview, listOfMeals }) {
 
   return (
     <div className="justify-center items-center p-10 flex flex-col max-h-max font-mono">
-      <NavLink to="/profilesettings">
-        <button className="border-4 border-red-400 bg-red-400 rounded-md text-sm">
-          {" "}
-          Edit User Settings
-        </button>
-      </NavLink>
-      <h3>Hi, {user.name}!</h3>
+      <h3 className="font-extrabold text-2xl">Hi, {user.name}!</h3>
       <img alt="avatar" src={user.image} width={100} height={100} />
-      <div className="border-2 border-gray-200 rounded-md">
+      <div className="p-4">
+        <NavLink to="/profilesettings">
+          <button className="border-4 border-red-400 bg-red-400 rounded-md text-sm">
+            Edit User Settings
+          </button>
+        </NavLink>
+      </div>
+      <div className="border-2 border-gray-200 rounded-md text-xl font-bold">
         <h3>Saved Meals List:</h3>
       </div>
       {mealShowList}
-
-      <div className="border-2 border-gray-200 rounded-md">
+      <div className="border-2 border-gray-200 rounded-md text-xl font-bold">
         <h3>Saved Workouts List:</h3>
       </div>
       {workOutShowList}
--- client/src/ProfileSettings.js
+++ client/src/ProfileSettings.js
@@ -1,33 +1,59 @@
 import { NavLink } from "react-router-dom";
 
 function ProfileSettings({ user, updateUserInfo }) {
+  //FUNCTION TO UPDATE USERS NAME AND PROFILE PIC
+  function updatingUserInfo(event) {
+    event.preventDefault();
+    user.name = event.target.nameofuser.value;
+    user.image = event.target.profilepic.value;
+    console.log(user);
+    console.log(event.target.nameofuser.value);
+    console.log(event.target.profilepic.value);
+    //SENDS THE USER INFO BACK UP TO APP TO HANDLE
+    updateUserInfo(user);
+  }
 
-    //FUNCTION TO UPDATE USERS NAME AND PROFILE PIC
-    function updatingUserInfo(event) {
-        event.preventDefault()
-        user.name = event.target.nameofuser.value
-        user.image = event.target.profilepic.value 
-        console.log(user)
-        console.log(event.target.nameofuser.value)
-        console.log(event.target.profilepic.value )
-        //SENDS THE USER INFO BACK UP TO APP TO HANDLE
-        updateUserInfo(user)
-    } 
-
-    return (
-        <div>
-            <form id="profile-setting-form" onSubmit={updatingUserInfo}>
-                <label htmlFor="nameofuser">Name: </label>
-                <input name="nameofuser" type="text" id="nameofuser" />
-                <label htmlFor="profilepic">Link to Image: </label>
-                <input name="profilepic" type="text" id="profilepic" />
-                <button type="submit">Save User Profile</button>
-                <NavLink to="/profile">
-                    <button>Go Back To Profile</button>
-                </NavLink>
-            </form>
+  return (
+    <div>
+      <form
+        className="flex flex-col items-center max-h-max font-mono"
+        id="profile-setting-form"
+        onSubmit={updatingUserInfo}
+      >
+        <div className="p-4">
+          <label htmlFor="nameofuser">Name: </label>
+          <input
+            className="border-2 border-red-500 rounded-md"
+            name="nameofuser"
+            type="text"
+            id="nameofuser"
+          />
         </div>
-    )
+        <div className="p-4">
+          <label htmlFor="profilepic">Link to Image: </label>
+          <input
+            className="border-2 border-red-500 rounded-md"
+            name="profilepic"
+            type="text"
+            id="profilepic"
+          />
+        </div>
+        <div className="p-4">
+          <button
+            className="bg-red-400 border-4 border-red-400 rounded-md"
+            type="submit"
+          >
+            Save User Profile
+          </button>
+        </div>
+        <NavLink to="/profile">
+          <button className="bg-red-400 border-4 border-red-400 rounded-md">
+            Go Back To Profile
+          </button>
+        </NavLink>
+      </form>
+    </div>
+  );
 }
 
-export default ProfileSettings
+export default ProfileSettings;

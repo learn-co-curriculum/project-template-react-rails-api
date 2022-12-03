2b0ff98c0111007d85a20a7dc018d9eb49d29f95
Joseph <joffan98@gmail.com>
2022-12-03 15:46:15 -0600

    finished loginscreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import { Routes , Route, BrowserRouter as Router} from "react-router-dom"
+import LoginForm from './LoginPage/LoginForm';
+import CreateAccountForm from "./CreateAccount/CreateAccountForm"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route 
+          exact path="/" 
+          element={<LoginForm/>}
+        />
+        <Route 
+          exact path="/create-account" 
+          element={<CreateAccountForm/>}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/CreateAccount/CreateAccountForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateAccountForm = () => {
+    return <></>
+}
+
+export default CreateAccountForm

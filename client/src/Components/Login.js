845b77a3d9818ac31e200721a9cdb4f120b72312
Noahkimtai <noahkimtai24@gmail.com>
2022-12-05 23:59:31 +0300

    Login component styled

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { renderMatches } from "react-router-dom";
+import styles from  '../Home.module.css'
 
 
 function Login(){
   return(
-    <div>
-      <form>
-        Login
-      </form>
-    </div>
+    <form className="container">
+      <p><b>Sign In to NyamaHaven</b></p>
+      <label for='username'><b>Username :</b></label>
+      <input type='text' placeholder="Enter Username" name="username"></input>
+
+      <label for='password'> Password</label>
+      <input type='password' placeholder='Enter password' name='password' required></input>
+      <button type='submit'>Login</button>
+      <br></br>
+      <span>Don't have an Account?</span><button> Sign Up</button>
+    </form>
   )
 
 }

e10c8f3883539f67bd4395dcece6d62967edbc85
Grace <grace941217@gmail.com>
2022-12-08 16:42:34 -0500

    signup page

---
+++ client/src/components/Signup.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+function SignUp() {
+    const [formData, setFormData] = useState({
+        username:'',
+        password:''
+    })
+    const [errors, setErrors] = useState([])
+    // const [isLiked, setIsLiked] = useState(false);
+    const {username, password} = formData
+    const navigate = useNavigate()
+
+    function onSubmit(e){
+        e.preventDefault()
+        navigate("/")
+        const user = {
+            username,
+            password
+        }
+       
+        fetch(`/users`, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(user)
+        })
+        .then(res => {
+            if(res.ok) {
+                res.json().then(user => {
+                    navigate(`/users/${user.id}`)
+                })
+            } else {
+                res.json().then(json => setErrors(Object.entries(json.errors)))
+            }
+        })
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+
+    return (
+        <> 
+            <div id ="signup-page" className="content"> 
+            <label>Username : </label>  
+            <input placeholder="Your Username..." type='text' name='username' value={username} onChange={handleChange} />
+        
+            <label>Password : </label>
+            <input placeholder="Your Password..." type='password' name='password' value={password} onChange={handleChange} />
+            
+            <input id="signup-form" className="button" type='submit' onSubmit={onSubmit}/>
+            {errors?errors.map(e => <div>{e[0]+': ' + e[1]}</div>):null}
+            </div>
+        </>
+
+    )
+}
+
+export default SignUp
